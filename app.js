06adb776743a88f49875e3387b2dcabb4b76a417
montshejohn <mokgokongjohannes@gmail.com>
2018-06-25 16:11:15 +0200

    add bodyParser and log data from the client side

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 var express = require("express");
-//var bodyParser = require("body-parser");
+var bodyParser = require("body-parser");
 
 var app = express();
+
+app.get("/data", (req, res) => {
+  console.log(req.body)
+});
+
+
 app.use(express.static("public"));
-app.get("/", (req, res) => {
-  res.send("testing");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.post("/data", (req, res) => {
+  res.send("Request success");
+  console.log(req.body)
 });
 
 app.listen(3003, () => {
-  console.log("server running on localhost:3000 ");
+  console.log("server running on localhost:3003 ");
 });
